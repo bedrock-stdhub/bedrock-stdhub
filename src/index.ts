64426fbbcd2f614ffa937abe77e7f390ca2983f3
Wesley F. Young <willamwei@outlook.com>
2024-06-24 09:20:11 +0800

    update: pre-running checks

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
 import express from 'express';
 import { spawn } from 'node:child_process';
-import fs from 'fs';
+import fs from 'node:fs';
 import * as path from 'node:path';
+import * as process from 'node:process';
+import os from 'node:os';
+import fileApiRouter from '@/api/file';
+import configRouter from '@/api/config';
+
+// check for platform
+let bdsCommand: string = '';
+switch (os.platform()) {
+  case 'win32': {
+    bdsCommand = '.\\bedrock_server.exe';
+    break;
+  }
+  case 'linux': {
+    bdsCommand = './bedrock_server';
+    break;
+  }
+  default: {
+    console.log(`The current platform ${os.platform()} is not supported.`);
+    process.exit(1);
+  }
+}
+
+// check for bedrock_server binaries
+if (!fs.existsSync('bedrock_server') && !fs.existsSync('bedrock_server.exe')) {
+  console.log('BDS binary file not found. Please check your installation.');
+  process.exit(1);
+}
 
 // Initialization begin
-
-export const pluginsRoot = path.resolve(__dirname, 'plugins');
+export const pluginsRoot = 'plugins';
 if (!fs.existsSync(pluginsRoot)) {
   fs.mkdirSync(pluginsRoot);
 }
@@ -14,25 +40,16 @@ if (!fs.existsSync(pluginsRoot)) {
 
 const app = express();
 app.use(express.json());
-
-import fileApiRouter from '@/api/file';
-import configRouter from '@/api/config';
-
 app.use('/file', fileApiRouter);
 app.use('/config', configRouter);
 
 const STDHUB_PORT = 29202;
-
 app.listen(STDHUB_PORT, () => {
   // console.log('Server started on *:29202');
   console.log('Starting BDS process...');
   console.log();
 
-  const bdsProcess = spawn(
-    './bedrock_server', // indicates that the executable should be at the same path with BDS binary
-    { stdio: 'inherit' }
-  );
-
+  const bdsProcess = spawn(bdsCommand, { stdio: 'inherit' });
   bdsProcess.on('close', (code) => {
     console.log('\x1b[0m');
     console.log('BDS process exited with code', code ?? 0);
