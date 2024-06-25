8888c951b765eecec0cab4a20798b5118f138140
Wesley F. Young <willamwei@outlook.com>
2024-06-25 22:14:41 +0800

    refactor: /config is now read-only except when the file does not exist.
    If you want your plugin to write something into storage, use `data` API instead.

--- src/api/config/index.ts
+++ src/api/config/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import registerAction from '@/utils/action';
 import readConfigAction from './read-config';
-import writeConfigAction from './write-config';
+// import writeConfigAction from './write-config';
 
 const router = express.Router();
 
-registerAction(router, '/read', readConfigAction);
-registerAction(router, '/write', writeConfigAction);
+registerAction(router, '/', readConfigAction);
+// registerAction(router, '/write', writeConfigAction);
 
 export default router;
