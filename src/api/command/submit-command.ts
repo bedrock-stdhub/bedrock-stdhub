393e40ebf4aedbdb382300432e36d68280dd6798
Wesley F. Young <willamwei@outlook.com>
2024-06-30 09:32:48 +0800

    refactor: add triggerCommand function for commands from terminal

--- src/api/command/submit-command.ts
+++ src/api/command/submit-command.ts
@@ -15,21 +15,25 @@ const schema = {
   additionalProperties: false,
 } as const satisfies JSONSchema;
 
+export function triggerCommand(commandString: string, playerId?: string) {
+  const resolved = resolveCommand(commandString);
+  if (!resolved) {
+    return { status: 404 };
+  } else {
+    triggerScriptEvent(resolved.namespace, new CommandDispatchEvent(
+      resolved.resolvedText,
+      playerId
+    ));
+    return {};
+  }
+}
+
 const submitCommandAction = {
   schema,
   handler: (params: FromSchema<typeof schema>) => {
     console.log(`Player ${params.playerName} tries to call plugin command ${params.commandString}`);
 
-    const resolved = resolveCommand(params.commandString);
-    if (!resolved) {
-      return { status: 404 };
-    } else {
-      triggerScriptEvent(resolved.namespace, new CommandDispatchEvent(
-        params.playerId,
-        resolved.resolvedText
-      ));
-      return {};
-    }
+    return triggerCommand(params.commandString, params.playerId);
   }
 } satisfies Action;
 
--- src/event/command/CommandDispatchEvent.ts
+++ src/event/command/CommandDispatchEvent.ts
@@ -3,7 +3,7 @@ import { ScriptEvent } from '@/event/ScriptEvent';
 export class CommandDispatchEvent extends ScriptEvent {
   readonly eventName = 'CommandDispatchEvent';
 
-  constructor(public playerId: string, public commandString: string) {
+  constructor(public commandString: string, public playerId?: string) {
     super();
   }
 }
